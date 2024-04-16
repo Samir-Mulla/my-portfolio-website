29327fa4475987d92cb2e395a976f46439514fd7
Samir Mulla <samirmulla710@gmail.com>
2024-04-16 10:02:16 +0530

    pdf : resume link added

--- src/components/Button/IconButton.jsx
+++ src/components/Button/IconButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-  import { RiFileTextLine } from "react-icons/ri";
-
+import { RiFileTextLine } from "react-icons/ri";
+import SamirResume from "/Samir-Resume-updated-2024.pdf"
 
 function IconButton() {
   return (
     <>
       <a
-        href="https://drive.google.com/file/d/1Gd6gy1f1DIvGS7rN7U1a8FENGYQRSD6q/view?usp=sharing"
+        href={SamirResume}
         rel="noopener noreferrer"
         target="_blank"
         className="flex items-center gap-2 italic border p-2 border-r-4 border-black rounded hover:bg-[#e7e7e7] hover:ease-in duration-300"
