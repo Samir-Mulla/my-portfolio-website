f96d0cad9ed4caf0afa396b2e28c097a8da7b272
Samir Mulla <samirmulla710@gmail.com>
2024-04-18 20:16:01 +0530

    component : file Deleted

--- src/hooks/usehandlePhoneNumberChange.jsx
+++
@@ -1,7 +0,0 @@
-export const handlePhoneNumberChange = (e) => {
-  const inputValue = e.target.value;
-  const numericValue = inputValue.replace(/\D/g, ""); // Remove non-numeric characters
-
-  // Update the input field value with only numeric characters
-  e.target.value = numericValue;
-};
