1e4285140c05ff70304c8297093064c75be821bb
Samir Mulla <samirmulla710@gmail.com>
2024-04-28 11:02:13 +0530

    component : code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from "react";
-// import Aos from "aos";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/UI/Hero";
@@ -8,17 +6,17 @@ import Skills from "./components/Skills/Skills";
 import PortfolioForm from "./components/PortfolioForm/PortfolioForm";
 import ScrollToTopButton from "./components/Button/ScrollToTopButton";
 import CustomCursor from "./components/customCursor/customCursor";
-
+import Services from "./components/Services/services";
 
 function App() {
-  
   return (
     <>
       <Header />
-      <CustomCursor />
       <Hero />
       <About />
       <Skills />
+      <Services />
+      <CustomCursor />
       <PortfolioForm />
       <ScrollToTopButton />
       <Footer />
