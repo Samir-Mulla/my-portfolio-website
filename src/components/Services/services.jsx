232966b076eb39989586f7eb71943e12e1c1fd96
Samir Mulla <samirmulla710@gmail.com>
2024-06-23 22:04:03 +0530

    component : code refactor

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -1,4 +1,3 @@
-// src/components/Services.js
 import React from "react";
 
 export default function Services() {
@@ -21,13 +20,13 @@ export default function Services() {
   ];
 
   return (
-    <section id="services" className="mt-32 scroll-mt-20 font-customFont">
-      <h1 className="text-center font-thin italic text-8xl">
+    <section id="services" className="mt-16 scroll-mt-20 font-customFont">
+      <h1 className="text-center font-thin italic text-4xl md:text-6xl lg:text-8xl font-customFont">
         My <span className="underline">Services</span> :
       </h1>
       <div className="px-5 py-10 mx-auto text-center lg:px-40 bg-green-100">
         <div className="flex flex-col w-full mb-12">
-          <p className="lg:w-2/3 mx-auto leading-normal text-4xl">
+          <p className=" mx-auto leading-normal text-4xl sm:text-5xl md:text-6xl lg:text-6xl lg:leading-normal">
             I provide a variety of services that can help you, grow your{" "}
             <span className="font-extrabold underline decoration-wavy underline-offset-4 italic text-[#ba0000] decoration-black">
               Business
@@ -35,17 +34,18 @@ export default function Services() {
             .
           </p>
         </div>
-        <div className="flex justify-between flex-wrap text-balance">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
           {services.map((service, index) => (
-            <div key={index} className="xl:w-1/4 md:w-1/2">
-              <div className="bg-black p-6 rounded-lg text-white shadow-[4px_7px_1px_1px_#ba0000] ">
-                <h2 className="text-3xl font-bold text-white mb-3">
-                  {service.title}
-                </h2>
-                <p className=" text-xl leading-relaxed mb-4 text-gray-300 italic">
-                  {service.description}
-                </p>
-              </div>
+            <div
+              key={index}
+              className="bg-black text-center p-6 rounded-lg text-white shadow-[4px_7px_1px_1px_#ba0000] "
+            >
+              <h2 className="text-xl sm:text-lg md:text-2xl lg:text-2xl xl:text-3xl font-bold text-white mb-3">
+                {service.title}
+              </h2>
+              <p className="text-xl sm:text-lg md:text-xl lg:text-lg xl:text-xl leading-relaxed mb-4 text-gray-300 italic">
+                {service.description}
+              </p>
             </div>
           ))}
         </div>
