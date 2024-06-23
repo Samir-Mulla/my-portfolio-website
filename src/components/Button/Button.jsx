affa332f0f369785e1fd1148d737272880533c86
Samir Mulla <samirmulla710@gmail.com>
2024-06-23 21:46:44 +0530

    component : written reusable code

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-  import { RiFileTextLine } from "react-icons/ri";
+import classNames from "classnames";
 
+export function Button({ variant, size, children, ...props }) {
+  const classes = classNames(
+    "btn",
+    {
+      "btn-outline": variant === "outline",
+      "btn-icon": size === "icon",
+    },
+    props.className
+  );
 
-function Button() {
   return (
-    <>
-      <a
-        href="https://drive.google.com/file/d/1Gd6gy1f1DIvGS7rN7U1a8FENGYQRSD6q/view?usp=sharing"
-        rel="noopener noreferrer"
-        target="_blank"
-        className="flex items-center gap-2 italic border p-2 border-r-4 border-black rounded hover:bg-[#e7e7e7] hover:ease-in duration-300"
-      >
-        <RiFileTextLine size="1.5rem" />
-        View My Resume
-      </a>
-    </>
+    <button className={classes} {...props}>
+      {children}
+    </button>
   );
 }
-
-export default Button;
