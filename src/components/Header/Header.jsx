651d2add256c7d228eb039946322d9b23ce1bc3e
Samir Mulla <samirmulla710@gmail.com>
2024-06-23 21:53:13 +0530

    component : code refactor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,33 @@
+// src/components/Header/Header.jsx
 import React from "react";
 import { RiMenuFoldFill } from "react-icons/ri";
 import Navbar from "../Navbar/Navbar";
 import Logo from "/Logo.svg";
 import IconButton from "../Button/IconButton";
+// import { FaMoon, FaSun } from "react-icons/fa"; // Import the moon and sun icons
 
 function Header() {
   return (
     <>
       <header>
-        <div className="h-20 w-full fixed bg-white flex justify-around selection:bg-yellow-300 font-customFont border-b">
-          <div className="flex items-center animate-bounce">
-            <a href=""><img src={Logo} alt="Logo" /></a>
+        {/* logo */}
+        <div className="bg-white h-20 w-full fixed flex justify-around selection:bg-yellow-300 font-customFont border-b">
+          <div className=" lg:block hidden md:hidden xl:flex items-center">
+            <a href="/home">
+              <img src={Logo} alt="Logo" />
+            </a>
           </div>
-
+          {/* navbar */}
           <Navbar />
-
           <div className="flex items-center">
             <IconButton />
           </div>
 
+          {/* mobile menu */}
           <button className="md:hidden">
             <RiMenuFoldFill size="2rem" />
           </button>
+          {/* Dark mode toggle */}
         </div>
       </header>
     </>
