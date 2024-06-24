30301147bd33b83bc8206249a8858e0fbbb1f778
Samir Mulla <samirmulla710@gmail.com>
2024-06-24 20:17:08 +0530

    component : new logo component created

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LogoIcon from "/Logo.svg";
+
+function Logo() {
+  return (
+    <>
+        <a href="/home">
+              <img src={LogoIcon} alt="Logo" />
+            </a>
+    </>
+  )
+}
+
+export default Logo;
+
+
