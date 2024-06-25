52d1898ba78e3bd5c05e3307898d47d510c2cfc4
Samir Mulla <samirmulla710@gmail.com>
2024-06-25 09:53:35 +0530

    config : setup is done

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "/Logo.svg", // Exclude Logo.svg from the build
-      ],
-    },
-  },
+  plugins: [
+    react(),
+    svgr(), // Add the svgr plugin
+  ],
 });
